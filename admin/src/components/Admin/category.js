56baa8f634a01952276cfaf6fd71d6bc9de43fed
fereszareb <fereszareb@gmail.com>
2022-03-09 21:51:55 +0100

    add category

--- admin/src/components/Admin/category.js
+++ admin/src/components/Admin/category.js
@@ -1,6 +1,46 @@
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+const data = JSON.parse("[{},{},{},{},{},{},{},{},{},{}]");
+const finalSpaceCharacters = [
+  {
+    id: "gary",
+    name: "Gary Goodspeed",
+    thumb: "/images/gary.png",
+  },
+  {
+    id: "cato",
+    name: "Little Cato",
+    thumb: "/images/cato.png",
+  },
+  {
+    id: "kvn",
+    name: "KVN",
+    thumb: "/images/kvn.png",
+  },
+  {
+    id: "mooncake",
+    name: "Mooncake",
+    thumb: "/images/mooncake.png",
+  },
+  {
+    id: "quinn",
+    name: "Quinn Ergon",
+    thumb: "/images/quinn.png",
+  },
+];
 const Category = () => {
+  const [characters, updateCharacters] = useState(finalSpaceCharacters);
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateCharacters(items);
+  }
   return (
     <div>
       <nav aria-label="breadcrumb">
@@ -13,6 +53,48 @@ const Category = () => {
           </li>
         </ol>
       </nav>
+      <div className="cardTemplate shadow-sm">
+        <div className="title-cardTemplate">
+          <h1>Category</h1>
+          <button className="btn">refresh</button>
+        </div>
+        <div className="content-cardTemplate">
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="characters">
+              {(provided) => (
+                <ul
+                  className="characters"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {characters.map(({ id, name }, index) => {
+                    return (
+                      <Draggable key={id} draggableId={id} index={index}>
+                        {(provided) => (
+                          <li
+                            className="item-Category"
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <div className="row">
+                              <div className="col-2">++</div>
+                              <div className="col-2">++</div>
+                              <div className="col-6">Category</div>
+                              <div className="col-2">icon</div>
+                            </div>
+                          </li>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                </ul>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </div>
+      </div>
     </div>
   );
 };
