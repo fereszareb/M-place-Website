57b15d410cc4e0c690b2260491f3bb6525d26c53
fereszareb <fereszareb@gmail.com>
2022-03-11 22:24:41 +0100

    update login

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -3,25 +3,30 @@ import axios from "axios";
 import { useHistory } from "react-router-dom";
 import "./../css/registration.css";
 import { Link } from "react-router-dom";
-
+import api from "./../config.service";
 const Register = () => {
   const history = useHistory();
   if (localStorage.getItem("token") !== null) {
     history.push("/");
   }
-
+  const [Submited, setSubmited] = useState(false);
+  const [MssageErreur, setMessageErreur] = useState("");
   const [registerInfo, setRegisterInfo] = useState({
     name: "",
     email: "",
     password: "",
     numTel: 0,
+    password_repeat: "",
+    licence: false,
   });
 
   const [errors, setErrors] = useState({
-    name: "",
-    email: "",
-    password: "",
-    numTel: 0,
+    name: false,
+    email: false,
+    password: false,
+    numTel: false,
+    password_repeat: false,
+    licence: false,
   });
 
   const regChangeHandler = (e) => {
@@ -29,126 +34,182 @@ const Register = () => {
       ...registerInfo,
       [e.target.name]: e.target.value,
     });
+    if (Submited) {
+      validateData();
+    }
   };
 
+  const regChangecheckBox = (e) => {
+    setRegisterInfo({
+      ...registerInfo,
+      [e.target.name]: e.target.checked,
+    });
+    if (Submited) {
+      validateData();
+    }
+  };
+
+  function validateData() {
+    !registerInfo.name ? (errors.name = true) : (errors.name = false);
+    !registerInfo.numTel || registerInfo.numTel.length !== 8
+      ? (errors.numTel = true)
+      : (errors.numTel = false);
+    registerInfo.password_repeat !== registerInfo.password ||
+    !registerInfo.password_repeat
+      ? (errors.password_repeat = true)
+      : (errors.password_repeat = false);
+    !registerInfo.email ? (errors.email = true) : (errors.email = false);
+    !registerInfo.password || registerInfo.password.length < 8
+      ? (errors.password = true)
+      : (errors.password = false);
+    registerInfo.licence ? (errors.licence = true) : (errors.licence = false);
+    setErrors(errors);
+  }
+
+  const [Success, setSuccess] = useState(false);
   const register = (e) => {
     e.preventDefault();
-    axios
-      .post(
-        "http://172.16.134.105:3000/api/v1/auth/Client/register",
-        registerInfo,
-        {
+    setSubmited(true);
+    validateData();
+    if (
+      errors.email === true ||
+      errors.name === true ||
+      errors.numTel === true ||
+      errors.password === true ||
+      errors.password_repeat === true /*||
+      errors.licence === true*/
+    ) {
+      setMessageErreur("All data is required!");
+    } else if (registerInfo.password_repeat !== registerInfo.password) {
+      setMessageErreur("Those passwords didn’t match. Try again.");
+      /*  } else if (!registerInfo.licence) {
+      setMessageErreur("Check the licence");*/
+    } else {
+      api
+        .post("api/v1/auth/Client/register", registerInfo, {
           withCredentials: true,
-        }
-      )
-      .then((res) => {
-        console.log("response from registering", res.status);
-        if (res.data.errors) {
-          setErrors(res.data.errors);
-        } else {
-          console.log("success!");
-          localStorage.setItem("token", res.data.token);
-          history.push("/login");
-        }
-      })
-      .catch((err) => console.log(err));
+        })
+        .then((res) => {
+          console.log("response from registering", res.status);
+          console.log(res.data);
+          if (res.data.errors) {
+            setMessageErreur(res.data.msg);
+          } else {
+            console.log("success!");
+            setSuccess(true);
+          }
+        })
+        .catch((err) => setMessageErreur("Client already exists"));
+    }
   };
 
   return (
     <section className="register">
-      <div className="form-container">
-        <div className="image-holder"></div>
-        <form onSubmit={register}>
-          <h2 className="text-center">
-            <strong>Create</strong> an account.
-          </h2>
-          <div className="mb-3">
-            <input
-              className="form-control"
-              type="text"
-              name="name"
-              onChange={regChangeHandler}
-              placeholder="Username"
-            />
-            {errors.name ? (
-              <p className="text-danger">{errors.name.message}</p>
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="mb-3">
-            <input
-              className="form-control"
-              type="email"
-              name="email"
-              onChange={regChangeHandler}
-              placeholder="Email"
-            />
-            {errors.email ? (
-              <p className="text-danger">{errors.email.message}</p>
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="mb-3">
-            <input
-              className="form-control"
-              type="password"
-              name="password"
-              onChange={regChangeHandler}
-              placeholder="Password"
-            />
-            {errors.password ? (
-              <p className="text-danger">{errors.password.message}</p>
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="mb-3">
-            <input
-              className="form-control"
-              type="password"
-              name="password-repeat"
-              placeholder="Password (repeat)"
-            />
-            {errors.confirm ? (
-              <p className="text-danger">{errors.confirm.message}</p>
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="mb-3">
-            <input
-              className="form-control"
-              type="text"
-              name="numTel"
-              onChange={regChangeHandler}
-              placeholder="Phone"
-            />
-            {errors.name ? (
-              <p className="text-danger">{errors.name.message}</p>
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="mb-3">
-            <div className="form-check">
-              <label className="form-check-label">
-                <input className="form-check-input" type="checkbox" />I agree to
-                the license terms.
-              </label>
+      {Success === false ? (
+        <div className="form-container">
+          <div className="image-holder"></div>
+          <form onSubmit={register}>
+            <h2 className="text-center">
+              <strong>Create</strong> an account.
+            </h2>
+            <div className="mb-3">
+              <input
+                className={"form-control " + (errors.name ? "erreur" : "")}
+                type="text"
+                name="name"
+                onChange={regChangeHandler}
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                className={"form-control " + (errors.email ? "erreur" : "")}
+                type="email"
+                name="email"
+                onChange={regChangeHandler}
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                className={"form-control " + (errors.password ? "erreur" : "")}
+                type="password"
+                name="password"
+                onChange={regChangeHandler}
+                placeholder="Password"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                className={
+                  "form-control " + (errors.password_repeat ? "erreur" : "")
+                }
+                type="password"
+                name="password_repeat"
+                onChange={regChangeHandler}
+                placeholder="Password (repeat)"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                className={"form-control " + (errors.numTel ? "erreur" : "")}
+                type="text"
+                name="numTel"
+                onChange={regChangeHandler}
+                placeholder="Phone"
+              />
+            </div>
+            <div className="mb-3">
+              <div className="form-check">
+                <label
+                  className={
+                    "form-check-label " +
+                    (errors.licence ? "erreurLicence" : "")
+                  }
+                >
+                  <input
+                    className={
+                      "form-check-input " +
+                      (errors.licence ? "erreurLicence" : "")
+                    }
+                    onChange={regChangecheckBox}
+                    type="checkbox"
+                    name="licence"
+                  />
+                  I agree to the license terms.
+                </label>
+              </div>
+            </div>
+            <p className="text-danger text-center">{MssageErreur}</p>
+            <div className="mb-3">
+              <button className="btn btn-primary d-block w-100" type="submit">
+                Sign Up
+              </button>
+            </div>
+            <Link className="already" to="./login">
+              You already have an account? Login here.
+            </Link>
+          </form>
+        </div>
+      ) : (
+        <div className="form-container">
+          <div className="messageEmailConfiramation">
+            <h2>Thanks for signing up, please confirm your email.</h2>
+            <p className="mt-2">
+              We've emailed you a confirmation link. Once you confirm your email
+              you can continue setting up your profile
+            </p>
+            <div className="text-center">
+              <Link
+                className="btn btn-primary d-block m-auto pe-5 px-5 mt-5 w-200px"
+                to="./login"
+              >
+                Log in
+              </Link>
             </div>
           </div>
-          <div className="mb-3">
-            <button className="btn btn-primary d-block w-100" type="submit">
-              Sign Up
-            </button>
-          </div>
-          <Link className="already" to="./login">
-            You already have an account? Login here.
-          </Link>
-        </form>
-      </div>
+        </div>
+      )}
     </section>
   );
 };
