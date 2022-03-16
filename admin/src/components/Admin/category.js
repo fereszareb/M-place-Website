cf2bf1c1bef634875a2faa7bfa282a5bb4ed27ec
fereszareb <fereszareb@gmail.com>
2022-03-16 12:30:12 +0100

    update category

--- admin/src/components/Admin/category.js
+++ admin/src/components/Admin/category.js
@@ -1,45 +1,142 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-const data = JSON.parse("[{},{},{},{},{},{},{},{},{},{}]");
-const finalSpaceCharacters = [
-  {
-    id: "gary",
-    name: "Gary Goodspeed",
-    thumb: "/images/gary.png",
-  },
-  {
-    id: "cato",
-    name: "Little Cato",
-    thumb: "/images/cato.png",
-  },
-  {
-    id: "kvn",
-    name: "KVN",
-    thumb: "/images/kvn.png",
-  },
-  {
-    id: "mooncake",
-    name: "Mooncake",
-    thumb: "/images/mooncake.png",
-  },
-  {
-    id: "quinn",
-    name: "Quinn Ergon",
-    thumb: "/images/quinn.png",
-  },
-];
+import { FaTrash, FaEdit } from "react-icons/fa";
+import { Offcanvas } from "react-bootstrap";
+
 const Category = () => {
-  const [characters, updateCharacters] = useState(finalSpaceCharacters);
+  const [dataToAdd, setDataToAdd] = useState({});
+  const [show, setShow] = useState(false);
+  const [data, setData] = useState([
+    {
+      id: 1,
+      Category: "Clothes",
+      child: [
+        {
+          id: 18,
+          Category: "Man Clothes",
+          child: [],
+          new: false,
+        },
+        {
+          id: 19,
+          Category: "Women Clothes",
+          child: [
+            {
+              id: 52,
+              Category: "Tshirt",
+              child: [],
+              new: false,
+            },
+            {
+              id: 53,
+              Category: "pants",
+              child: [],
+              new: false,
+            },
+            {
+              id: 54,
+              Category: "shoes",
+              child: [],
+              new: false,
+            },
+          ],
+          new: false,
+        },
+        {
+          id: 20,
+          Category: "Kids Clothes",
+          child: [],
+          new: false,
+        },
+      ],
+      new: false,
+    },
+    {
+      id: 2,
+      Category: "Technology",
+      child: [],
+      new: false,
+    },
+    {
+      id: 3,
+      Category: "Dogs & Cats",
+      child: [],
+      new: false,
+    },
+    {
+      id: 4,
+      Category: "House & Garden",
+      child: [],
+      new: false,
+    },
+    {
+      id: 5,
+      Category: "Event",
+      child: [],
+      new: false,
+    },
+  ]);
 
-  function handleOnDragEnd(result) {
-    if (!result.destination) return;
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  function deleteCategory(idCategory, idSousCategory, idSousSousCategory) {
+    var newdata = data;
+    setData([]);
+    if (idCategory !== null) {
+      const posCateg = newdata.findIndex((categ) => categ.id === idCategory);
+      if (idSousCategory !== null) {
+        const posSousCateg = newdata[posCateg].child.findIndex(
+          (categ) => categ.id === idSousCategory
+        );
+        if (idSousSousCategory !== null) {
+          const newCategory = newdata[posCateg].child[
+            posSousCateg
+          ].child.filter((child) => {
+            return child.id !== idSousSousCategory;
+          });
+          newdata[posCateg].child[posSousCateg].child = newCategory;
+          console.log(newdata);
+        } else {
+          const newCategory = newdata[posCateg].child.filter((child) => {
+            return child.id !== idSousCategory;
+          });
+          newdata[posCateg].child = newCategory;
+          console.log(newdata);
+        }
+      } else {
+        newdata = newdata.filter((child) => {
+          return child.id !== idCategory;
+        });
+        console.log(newdata);
+      }
+    }
 
-    const items = Array.from(characters);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    updateCharacters(items);
+    setData(newdata);
+  }
+  function editCategory(idCategory, idSousCategory, idSousSousCategory) {
+    var idToDelete;
+    if (idCategory !== null) {
+      const category = data.find((categ) => categ.id === idCategory);
+      idToDelete = category.id;
+      if (idSousCategory !== null) {
+        const sousCategory = category.child.find(
+          (categ) => categ.id === idSousCategory
+        );
+        idToDelete = sousCategory.id;
+        if (idSousSousCategory !== null) {
+          const sousSousCategory = sousCategory.child.find(
+            (categ) => categ.id === idSousSousCategory
+          );
+          idToDelete = sousSousCategory.id;
+        }
+      }
+    }
+    console.log(idToDelete);
+  }
+  function addCategory() {}
+  function btnAddCateg(id) {
+    setDataToAdd({ id: id });
+    handleShow();
   }
   return (
     <div>
@@ -53,48 +150,161 @@ const Category = () => {
           </li>
         </ol>
       </nav>
+      <button className="btn btn-primary" onClick={handleShow}>
+        Launch
+      </button>
       <div className="cardTemplate shadow-sm">
         <div className="title-cardTemplate">
           <h1>Category</h1>
           <button className="btn">refresh</button>
         </div>
         <div className="content-cardTemplate">
-          <DragDropContext onDragEnd={handleOnDragEnd}>
-            <Droppable droppableId="characters">
-              {(provided) => (
-                <ul
-                  className="characters"
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {characters.map(({ id, name }, index) => {
-                    return (
-                      <Draggable key={id} draggableId={id} index={index}>
-                        {(provided) => (
-                          <li
-                            className="item-Category"
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                          >
-                            <div className="row">
-                              <div className="col-2">++</div>
-                              <div className="col-2">++</div>
-                              <div className="col-6">Category</div>
-                              <div className="col-2">icon</div>
+          <div className="w-category m-auto">
+            {data.map((item, key) => {
+              return (
+                <div className="shadow rounded mb-3" key={key}>
+                  <div
+                    className={
+                      item.child.length === 0
+                        ? "item-Category blue"
+                        : "item-Category blue item-CategoryPLus collapsed"
+                    }
+                    data-bs-toggle="collapse"
+                    data-bs-target={"#target" + item.id}
+                  >
+                    <p className="titleCtagory d-inline-block">
+                      {item.Category}
+                    </p>
+                    <div className="icons d-inline">
+                      <FaTrash
+                        className="icon iconTrash"
+                        onClick={() => {
+                          deleteCategory(item.id, null, null);
+                        }}
+                      />
+                      <FaEdit
+                        className="icon iconEdit"
+                        onClick={() => {
+                          editCategory(item.id, null, null);
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="collapse" id={"target" + item.id}>
+                    <div className="p-3">
+                      {item.child.map((sousitem, key) => {
+                        return (
+                          <div className="shadow rounded mb-3" key={key}>
+                            <div
+                              className={
+                                sousitem.child.length === 0
+                                  ? "item-Category orange"
+                                  : "item-Category orange item-CategoryPLus collapsed"
+                              }
+                              data-bs-toggle="collapse"
+                              data-bs-target={"#target" + sousitem.id}
+                            >
+                              <p className="titleCtagory d-inline-block">
+                                {sousitem.Category}
+                              </p>
+                              <div className="icons d-inline">
+                                <FaTrash
+                                  className="icon iconTrash"
+                                  onClick={() => {
+                                    deleteCategory(item.id, sousitem.id, null);
+                                  }}
+                                />
+                                <FaEdit
+                                  className="icon iconEdit"
+                                  onClick={() => {
+                                    editCategory(item.id, sousitem.id, null);
+                                  }}
+                                />
+                              </div>
                             </div>
-                          </li>
-                        )}
-                      </Draggable>
-                    );
-                  })}
-                  {provided.placeholder}
-                </ul>
-              )}
-            </Droppable>
-          </DragDropContext>
+                            <div
+                              className="collapse"
+                              id={"target" + sousitem.id}
+                            >
+                              <div className="p-3">
+                                {sousitem.child.map((sousitem2, key) => {
+                                  return (
+                                    <div
+                                      className="shadow rounded mb-3"
+                                      key={key}
+                                    >
+                                      <div className="item-Category collapsed">
+                                        <p className="titleCtagory d-inline-block">
+                                          {sousitem2.Category}
+                                        </p>
+                                        <div className="icons d-inline">
+                                          <FaTrash
+                                            className="icon iconTrash"
+                                            onClick={() => {
+                                              deleteCategory(
+                                                item.id,
+                                                sousitem.id,
+                                                sousitem2.id
+                                              );
+                                            }}
+                                          />
+                                          <FaEdit
+                                            className="icon iconEdit"
+                                            onClick={() => {
+                                              editCategory(
+                                                item.id,
+                                                sousitem.id,
+                                                sousitem2.id
+                                              );
+                                            }}
+                                          />
+                                        </div>
+                                      </div>
+                                    </div>
+                                  );
+                                })}
+                                <div class="text-end">
+                                  <button className="btn-add blue shadow">
+                                    Add
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        );
+                      })}
+                      <div class="text-end">
+                        <button
+                          className="btn-add blue shadow"
+                          onClick={() => {
+                            btnAddCateg(item.id);
+                          }}
+                        >
+                          Add
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+            <div class="text-end">
+              <button className="btn-add blue shadow">Add</button>
+            </div>
+          </div>
         </div>
       </div>
+      <Offcanvas show={show} onHide={handleClose} placement={"end"}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Add a Category</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <form onSubmit={addCategory}>
+            <input type="text" name="name" placeholder="Name of category" />
+            id of parent : {dataToAdd.id}
+          </form>
+        </Offcanvas.Body>
+      </Offcanvas>
     </div>
   );
 };
