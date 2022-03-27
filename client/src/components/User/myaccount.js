bc1a3ff4bdb50b314fd349aecae0c15e471a9e85
fereszareb <fereszareb@gmail.com>
2022-03-27 16:51:25 +0200

    add my account to route

--- client/src/components/User/myaccount.js
+++ client/src/components/User/myaccount.js
@@ -1,7 +1,12 @@
 const Myaccount = () => {
   return (
-    <div>
-      <p>my account</p>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-3">gdhd</div>
+        <div className="col-3">jdjd</div>
+        <div className="col-3">dj</div>
+        <div className="col-3">hdj</div>
+      </div>
     </div>
   );
 };
