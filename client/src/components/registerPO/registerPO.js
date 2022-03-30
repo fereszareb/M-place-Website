294ec3a424cbccb345e3d7be997186151fab47e3
fereszareb <fereszareb@gmail.com>
2022-03-30 07:10:50 +0200

    update registerPO

--- client/src/components/registerPO/registerPO.js
+++ client/src/components/registerPO/registerPO.js
@@ -1,32 +1,34 @@
-import { Container, Row, Col } from "react-bootstrap";
 import { useState } from "react";
 import StepOne from "./stepOne.js";
 import StepTwo from "./StepTwo.js";
 import Final from "./Final";
-
+import StepThree from "./StepThree.js";
+import api from "./../../config.service";
 function RegisterPO() {
   //state for steps
-  const [step, setstep] = useState(2);
+  const [step, setstep] = useState(1);
 
   //state for form data
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
     email: "",
-    nameEntreprise: "",
-    password: "",
-    logo: "",
     phone: "",
-    pays: "",
+    cin: "",
+    cinFile: File,
+
+    nameEntreprise: "",
+    logo: File,
+    address: "",
+    country: "",
     ville: "",
-    codePostale: "",
+    codePostal: "",
     datofCreation: "",
     identifiantFiscale: "",
-    fileidentifiantFiscale: "",
-    identityType: "",
-    fileidentityCard: "",
-    adressse: "",
+    fileidentifiantFiscale: File,
     RNENumber: "",
+
+    password: "",
   });
 
   // function for going to next step by increasing step state by 1
@@ -39,66 +41,81 @@ function RegisterPO() {
     setstep(step - 1);
   };
 
-  // handling form input data by taking onchange value and updating our previous form data state
-  const handleInputData = (input) => (e) => {
-    // input value from the form
-    const { value } = e.target;
-
-    //updating for data state taking previous state and then adding new value to create new object
+  const registerChangeHandler = (e) => {
     setFormData((prevState) => ({
       ...prevState,
-      [input]: value,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const registerFileChangeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.files[0],
     }));
   };
 
+  const submitSignUP = () => {
+    api
+      .post("/PO/register", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
   // javascript switch case to show different form in each step
   switch (step) {
     // case 1 to show stepOne form and passing nextStep, prevStep, and handleInputData as handleFormData method as prop and also formData as value to the fprm
     case 1:
       return (
-        <div className="App">
-          <Container>
-            <Row>
-              <Col md={{ span: 6, offset: 3 }} className="custom-margin">
-                <StepOne
-                  nextStep={nextStep}
-                  handleFormData={handleInputData}
-                  values={formData}
-                />
-              </Col>
-            </Row>
-          </Container>
+        <div className="row justify-content-center mb-5 w-100 mx-0">
+          <div className="col-12 col-md-10 col-lg-9 col-xl-8 col-xxl-6">
+            <StepOne
+              nextStep={nextStep}
+              registerChangeHandler={registerChangeHandler}
+              registerFileChangeHandler={registerFileChangeHandler}
+              values={formData}
+            />
+          </div>
         </div>
       );
     // case 2 to show stepTwo form passing nextStep, prevStep, and handleInputData as handleFormData method as prop and also formData as value to the fprm
     case 2:
       return (
-        <div className="App">
-          <Container>
-            <Row>
-              <Col md={{ span: 6, offset: 3 }} className="custom-margin">
-                <StepTwo
-                  nextStep={nextStep}
-                  prevStep={prevStep}
-                  handleFormData={handleInputData}
-                  values={formData}
-                />
-              </Col>
-            </Row>
-          </Container>
+        <div className="row justify-content-center mb-5 w-100 mx-0">
+          <div className="col-12 col-md-10 col-lg-9 col-xl-8 col-xxl-6">
+            <StepTwo
+              nextStep={nextStep}
+              prevStep={prevStep}
+              registerChangeHandler={registerChangeHandler}
+              registerFileChangeHandler={registerFileChangeHandler}
+              values={formData}
+            />
+          </div>
         </div>
       );
     // Only formData is passed as prop to show the final value at form submit
     case 3:
       return (
-        <div className="App">
-          <Container>
-            <Row>
-              <Col md={{ span: 6, offset: 3 }} className="custom-margin">
-                <Final values={formData} />
-              </Col>
-            </Row>
-          </Container>
+        <div className="row justify-content-center mb-5 w-100 mx-0">
+          <div className="col-12 col-md-10 col-lg-9 col-xl-8 col-xxl-6">
+            <StepThree
+              nextStep={nextStep}
+              prevStep={prevStep}
+              registerChangeHandler={registerChangeHandler}
+              submitSignUP={submitSignUP}
+              values={formData}
+            />
+          </div>
+        </div>
+      );
+
+    case 4:
+      return (
+        <div className="row justify-content-center mb-5 w-100 mx-0">
+          <div className="col-12 col-md-10 col-lg-9 col-xl-8 col-xxl-6">
+            <Final values={formData} />
+          </div>
         </div>
       );
     // default case to show nothing
