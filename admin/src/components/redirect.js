b3cc487aa9c2d1116755b0e122339f6f87b47cb9
fereszareb <fereszareb@gmail.com>
2022-03-30 07:10:20 +0200

    update

--- admin/src/components/redirect.js
+++ admin/src/components/redirect.js
@@ -4,8 +4,8 @@ import { useParams, useHistory } from "react-router-dom";
 const Redirect = () => {
   const history = useHistory();
   const { token, refreshToken } = useParams();
-  localStorage.setItem("token", token);
-  localStorage.setItem("refreshToken", refreshToken);
+  localStorage.setItem("access_token", token);
+  localStorage.setItem("refresh_token", refreshToken);
   history.push("/");
   return null;
 };
