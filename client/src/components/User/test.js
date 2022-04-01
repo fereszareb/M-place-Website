e31a5ea0aead12e56e055612dfa065eeed068597
fereszareb <fereszareb@gmail.com>
2022-04-01 20:03:22 +0200

    file for testing

---
+++ client/src/components/User/test.js
@@ -0,0 +1,46 @@
+import io from "socket.io-client";
+import { useState } from "react";
+import "./file.css";
+import Messages from "./messages";
+const socket = io.connect("http://localhost:5000");
+
+const Test = () => {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Messages socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+};
+
+export default Test;
