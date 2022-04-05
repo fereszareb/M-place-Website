1febe3064395366c1b74bb6f3dda4f63fd1765f4
fereszareb <fereszareb@gmail.com>
2022-04-05 21:56:51 +0200

    aff function upload file

--- client/src/components/registerPO/registerPO.js
+++ client/src/components/registerPO/registerPO.js
@@ -5,6 +5,11 @@ import Final from "./Final";
 import StepThree from "./StepThree.js";
 import api from "./../../config.service";
 function RegisterPO() {
+  const [loadingCin, setLoadingCin] = useState(false);
+  const [loadinglogo, setLoadinglogo] = useState(false);
+  const [loadingfileidentifiantFiscale, setLoadingfileidentifiantFiscale] =
+    useState(false);
+  const [loadingSubmit, setLoadingSubmit] = useState(false);
   //state for steps
   const [step, setstep] = useState(1);
 
@@ -15,22 +20,21 @@ function RegisterPO() {
     email: "",
     phone: "",
     cin: "",
-    cinFile: File,
+    cinFile: "",
 
     nameEntreprise: "",
-    logo: File,
+    logo: "",
     address: "",
     country: "",
     ville: "",
     codePostal: "",
     datofCreation: "",
     identifiantFiscale: "",
-    fileidentifiantFiscale: File,
+    fileidentifiantFiscale: "",
     RNENumber: "",
 
     password: "",
   });
-
   // function for going to next step by increasing step state by 1
   const nextStep = () => {
     setstep(step + 1);
@@ -47,20 +51,73 @@ function RegisterPO() {
       [e.target.name]: e.target.value,
     }));
   };
+
+  function displayLoading(variable, ok) {
+    switch (variable) {
+      case "cinFile":
+        setLoadingCin(ok);
+        break;
+      case "logo":
+        setLoadinglogo(ok);
+        break;
+      case "fileidentifiantFiscale":
+        setLoadingfileidentifiantFiscale(ok);
+        break;
+      default:
+    }
+  }
+
   const registerFileChangeHandler = (e) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.files[0],
-    }));
+    displayLoading(e.target.name, true);
+    var bodyFormData = new FormData();
+    bodyFormData.append("", e.target.files[0]);
+    api
+      .post("/img/upload", bodyFormData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        setFormData((prevState) => ({
+          ...prevState,
+          [e.target.name]: res.data.data.imageUrl,
+        }));
+        displayLoading(e.target.name, false);
+      });
   };
 
   const submitSignUP = () => {
+    setLoadingSubmit(true);
+    const dataSend = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      company_name: formData.nameEntreprise,
+      company_email: formData.email,
+      password: formData.password,
+      logo_url: formData.logo,
+      country: formData.country,
+      city: formData.ville,
+      state: "Tunis",
+      zip_code: formData.codePostal,
+      address: formData.address,
+      professional_phone_number: formData.phone,
+      verification: 0,
+      creation_date: formData.datofCreation,
+      tax_ID_number: formData.identifiantFiscale,
+      tax_ID_card: formData.fileidentifiantFiscale,
+      owner_ID_type: formData.cinFile,
+      owner_ID: formData.cin,
+      RNE_number: formData.RNENumber,
+    };
+
     api
-      .post("/PO/register", formData, {
-        headers: { "Content-Type": "multipart/form-data" },
-      })
+      .post("/api/v1/auth/PO/register", dataSend)
       .then((res) => {
-        console.log(res);
+        setLoadingSubmit(false);
+        nextStep();
+      })
+      .catch((err) => {
+        alert("Erreur");
       });
   };
   // javascript switch case to show different form in each step
@@ -75,6 +132,7 @@ function RegisterPO() {
               registerChangeHandler={registerChangeHandler}
               registerFileChangeHandler={registerFileChangeHandler}
               values={formData}
+              loadingCin={loadingCin}
             />
           </div>
         </div>
@@ -90,6 +148,8 @@ function RegisterPO() {
               registerChangeHandler={registerChangeHandler}
               registerFileChangeHandler={registerFileChangeHandler}
               values={formData}
+              loadingfileidentifiantFiscale={loadingfileidentifiantFiscale}
+              loadinglogo={loadinglogo}
             />
           </div>
         </div>
@@ -105,6 +165,7 @@ function RegisterPO() {
               registerChangeHandler={registerChangeHandler}
               submitSignUP={submitSignUP}
               values={formData}
+              loadingSubmit={loadingSubmit}
             />
           </div>
         </div>
