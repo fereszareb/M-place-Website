6a589242b9ed2543f03477a17f114bd7c10508ce
fereszareb <fereszareb@gmail.com>
2022-04-06 05:55:52 +0200

    update template PO

--- ProductOwner/src/components/template.js
+++ ProductOwner/src/components/template.js
@@ -2,19 +2,12 @@ import logo from "./../logo.svg";
 import React, { Component } from "react";
 import { AiOutlineMenuUnfold } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import Users from "./Admin/users";
 import Products from "./Admin/product";
 import Events from "./Admin/events";
 import DashboardAdmin from "./Admin/dashboard";
-import ProductOwnerNotVerified from "./Admin/productOwner/ProductOwnerNotVerified";
-import ProductOwnerBlocked from "./Admin/productOwner/ProductOwnerBlocked";
-import ProductOwnerDeleted from "./Admin/productOwner/ProductOwnerDeleted";
 import Messages from "./Admin/message";
-import Category from "./Admin/category";
 import Reports from "./Admin/report";
-import Blogs from "./Admin/blogs";
-import ProductOwner from "./Admin/productOwner/ProductOwner";
-import { BiSubdirectoryRight } from "react-icons/bi";
+import Profile from "./Admin/profile.js";
 import "bootstrap/dist/js/bootstrap.js";
 function menuClick() {
   const elementContent = document.querySelector(".content");
@@ -35,26 +28,14 @@ export default class Template extends Component {
           return <Products />;
         case "dashboard":
           return <DashboardAdmin />;
-        case "users":
-          return <Users />;
-        case "category":
-          return <Category />;
-        case "productOwner":
-          return <ProductOwner />;
-        case "productOwnerNotVerified":
-          return <ProductOwnerNotVerified />;
-        case "productOwnerBlocked":
-          return <ProductOwnerBlocked />;
-        case "productOwnerDeleted":
-          return <ProductOwnerDeleted />;
         case "messages":
           return <Messages />;
         case "reports":
           return <Reports />;
         case "events":
           return <Events />;
-        case "blogs":
-          return <Blogs />;
+        case "profile":
+          return <Profile />;
 
         default:
           return <h1>No project match</h1>;
@@ -88,81 +69,10 @@ export default class Template extends Component {
                 }
               >
                 <Link to="/product">
-                  <p>Product</p>
+                  <p>Products</p>
                 </Link>
               </li>
 
-              <li
-                className={
-                  window.location.pathname.split("/")[1] === "users"
-                    ? "item active"
-                    : "item"
-                }
-              >
-                <Link to="/users">
-                  <p>Users</p>
-                </Link>
-              </li>
-
-              <li
-                className={
-                  window.location.pathname.split("/")[1] === "category"
-                    ? "item active"
-                    : "item"
-                }
-              >
-                <Link to="/category">
-                  <p>Category</p>
-                </Link>
-              </li>
-
-              <li
-                className={
-                  [
-                    "productOwner",
-                    "productOwnerNotVerified",
-                    "productOwnerBlocked",
-                  ].includes(window.location.pathname.split("/")[1])
-                    ? "item active"
-                    : "item"
-                }
-              >
-                <p data-bs-toggle="collapse" data-bs-target="#productOwner">
-                  Product Owner
-                </p>
-                <div className="collapse" id="productOwner">
-                  <ul>
-                    <li>
-                      <Link to="/productOwner">
-                        <p>
-                          <BiSubdirectoryRight /> All Product Owner
-                        </p>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/productOwnerNotVerified">
-                        <p>
-                          <BiSubdirectoryRight /> PO not verified
-                        </p>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/productOwnerBlocked">
-                        <p>
-                          <BiSubdirectoryRight /> PO blocked
-                        </p>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/productOwnerDeleted">
-                        <p>
-                          <BiSubdirectoryRight /> PO Deleted
-                        </p>
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-              </li>
               <li
                 className={
                   window.location.pathname.split("/")[1] === "messages"
@@ -201,13 +111,13 @@ export default class Template extends Component {
 
               <li
                 className={
-                  window.location.pathname.split("/")[1] === "blogs"
+                  window.location.pathname.split("/")[1] === "profile"
                     ? "item active"
                     : "item"
                 }
               >
-                <Link to="/blogs">
-                  <p>Blogs</p>
+                <Link to="/profile">
+                  <p>Profile</p>
                 </Link>
               </li>
             </ul>
