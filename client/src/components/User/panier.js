68c649fc3fd46f9ccc29bee3d49230389442923c
fereszareb <fereszareb@gmail.com>
2022-04-17 06:57:32 +0100

    template checkout

--- client/src/components/User/panier.js
+++ client/src/components/User/panier.js
@@ -1,7 +1,17 @@
 const Panier = () => {
   return (
-    <div>
-      <p>Panier</p>
+    <div className="row justify-content-center mb-5 w-100 mx-0">
+      <div className="col-12 col-md-11 col-lg-10">
+        <div className="text-center titleCarousel">
+          <b>Checkout</b>
+        </div>
+        <div className="card mt-5">
+          <div className="row">
+            <div className="col-12 col-lg-8"></div>
+            <div className="col-12 col-lg-4"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
