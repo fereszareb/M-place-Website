b9053ed68b9cd99a5d93985d6e347c7103bcc0f6
fereszareb <fereszareb@gmail.com>
2022-04-18 02:17:24 +0100

    responsive

--- client/src/components/User/panier.js
+++ client/src/components/User/panier.js
@@ -1,5 +1,6 @@
 import "./../../css/panier.css";
 import { MdDelete } from "react-icons/md";
+import { useState } from "react";
 const Panier = () => {
   const deleteProduct = (e) => {
     console.log(e.target.getAttribute("data"));
@@ -10,7 +11,7 @@ const Panier = () => {
   const productMinus = (e) => {
     console.log(e.target.getAttribute("data"));
   };
-  const productFromLocalStorage = [
+  const [productFromLocalStorage, setproductFromLocalStorage] = useState([
     {
       id: "1254",
       name: "productName number one ",
@@ -29,17 +30,17 @@ const Panier = () => {
       reduction: "0",
       nbrProduct: 1,
     },
-  ];
+  ]);
   return (
     <div className="row justify-content-center mb-5 w-100 mx-0">
-      <div className="col-12 col-md-11 col-lg-10">
+      <div className="col-12 col-xl-10">
         <div className="text-center titleCarousel">
           <b>Checkout</b>
         </div>
         <div className="card mt-5">
           <div className="row">
             <div className="col-12 col-lg-8">
-              <div className="card m-3">
+              <div className="card m-3 me-lg-0">
                 <div className="card-header">
                   {"Panier ( " + productFromLocalStorage.length + " )"}
                 </div>
@@ -51,25 +52,25 @@ const Panier = () => {
                         key={key}
                       >
                         <div className="row">
-                          <div className="col-6 col-md-3">
+                          <div className="col-12 col-sm-6 col-md-3 text-center">
                             <div
-                              className="product-img"
+                              className="product-img m-auto ms-md-0"
                               style={{
                                 backgroundImage: "url(" + product.img + ")",
                               }}
                             ></div>
                           </div>
-                          <div className="col-6 col-md-5 pt-3 size0-5em">
+                          <div className="col-12 col-sm-6 col-md-5 pt-3 size0-5em text-center">
                             <h6>{product.name}</h6>
                             <small>{"[" + product.sku + "]"}</small>
                           </div>
-                          <div className="col-6 col-md-4 text-end">
+                          <div className="col-12 col-6 col-md-4 text-center text-md-end">
                             {product.reduction === "0" ? (
-                              <div className="row pt-4">
+                              <div className="row pt-4 mb-3">
                                 <h5>{product.price} TND</h5>
                               </div>
                             ) : (
-                              <div className="row">
+                              <div className="row text-center mb-3">
                                 <h5>
                                   {(product.price * (100 - product.reduction)) /
                                     100 +
@@ -124,9 +125,22 @@ const Panier = () => {
               </div>
             </div>
             <div className="col-12 col-lg-4">
-              <div className="card m-3">
-                <div className="card-header">Checkout</div>
-                <div className="card-body"></div>
+              <div className="card m-3 ms-lg-0">
+                <div className="card-header">Total</div>
+                <div className="card-body">
+                  <div className="d-flex justify-content-between">
+                    <p>Subtotal</p>
+                    <p>200 TND</p>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <p>Discout</p>
+                    <p>12.5 TND</p>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <p>Coupon</p>
+                    <p>0 TND</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
