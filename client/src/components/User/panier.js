0518b957fbfe1de0b4a2f500f6815b1679980be9
fereszareb <fereszareb@gmail.com>
2022-04-18 06:45:45 +0100

    update

--- client/src/components/User/panier.js
+++ client/src/components/User/panier.js
@@ -89,120 +89,131 @@ const Panier = () => {
           <b>Checkout</b>
         </div>
         <div className="card mt-5">
-          <div className="row">
-            <div className="col-12 col-lg-8">
-              <div className="card m-3 me-lg-0">
-                <div className="card-header">
-                  {"Panier ( " + productFromLocalStorage.length + " )"}
-                </div>
-                <div className="card-body">
-                  {productFromLocalStorage.map((product, key) => {
-                    return (
-                      <div
-                        className="Item-product-card m-2 pt-2 pb-2 bb-1"
-                        key={key}
-                      >
-                        <div className="row">
-                          <div className="col-12 col-sm-6 col-md-3 text-center">
-                            <div
-                              className="product-img m-auto ms-md-0"
-                              style={{
-                                backgroundImage: "url(" + product.img + ")",
-                              }}
-                            ></div>
-                          </div>
-                          <div className="col-12 col-sm-6 col-md-5 pt-3 size0-5em text-center">
-                            <h6>{product.name}</h6>
-                            <small>{"[" + product.sku + "]"}</small>
-                          </div>
-                          <div className="col-12 col-6 col-md-4 text-center text-md-end">
-                            {product.reduction === "0" ? (
-                              <div className="row pt-4 mb-3">
-                                <h5>{product.price} TND</h5>
-                              </div>
-                            ) : (
-                              <div className="row mb-3">
-                                <h5>
-                                  {(product.price * (100 - product.reduction)) /
-                                    100 +
-                                    " "}
-                                  TND
-                                </h5>
-                                <div>
-                                  <del>
-                                    {product.price + " "}
-                                    TND
-                                  </del>
-                                  <span class="badge bg-warning m-0 ms-2">
-                                    {product.reduction}%
-                                  </span>
+          {productFromLocalStorage.length > 0 ? (
+            <div className="row">
+              <div className="col-12 col-lg-8">
+                <div className="card m-3 me-lg-0">
+                  <div className="card-header">
+                    {"Panier ( " + productFromLocalStorage.length + " )"}
+                  </div>
+                  <div className="card-body">
+                    {productFromLocalStorage.map((product, key) => {
+                      return (
+                        <div
+                          className="Item-product-card m-2 pt-2 pb-2 bb-1"
+                          key={key}
+                        >
+                          <div className="row">
+                            <div className="col-12 col-sm-6 col-md-3 text-center">
+                              <div
+                                className="product-img m-auto ms-md-0"
+                                style={{
+                                  backgroundImage: "url(" + product.img + ")",
+                                }}
+                              ></div>
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-5 pt-3 size0-5em text-center">
+                              <h6>{product.name}</h6>
+                              <small>{"[" + product.sku + "]"}</small>
+                            </div>
+                            <div className="col-12 col-6 col-md-4 text-center text-md-end">
+                              {product.reduction === "0" ? (
+                                <div className="row pt-4 mb-3">
+                                  <h5>{product.price} TND</h5>
                                 </div>
+                              ) : (
+                                <div className="row mb-3">
+                                  <h5>
+                                    {(product.price *
+                                      (100 - product.reduction)) /
+                                      100 +
+                                      " "}
+                                    TND
+                                  </h5>
+                                  <div>
+                                    <del>
+                                      {product.price + " "}
+                                      TND
+                                    </del>
+                                    <span class="badge bg-warning m-0 ms-2">
+                                      {product.reduction}%
+                                    </span>
+                                  </div>
+                                </div>
+                              )}
+                            </div>
+                            <div className="col-6">
+                              <button
+                                data={product.id}
+                                className="btn deleteBTN"
+                                onClick={deleteProduct}
+                              >
+                                <MdDelete /> Delete
+                              </button>
+                            </div>
+                            <div className="col-6 text-end">
+                              <div className="d-flex justify-content-end">
+                                <button
+                                  data={product.id}
+                                  className="btn add-minus"
+                                  onClick={productMinus}
+                                  disabled={product.nbrProduct === 1}
+                                >
+                                  -
+                                </button>
+                                <p className="ms-3 me-3">
+                                  {product.nbrProduct}{" "}
+                                </p>
+                                <button
+                                  data={product.id}
+                                  className="btn add-minus"
+                                  onClick={productPlus}
+                                >
+                                  +
+                                </button>
                               </div>
-                            )}
-                          </div>
-                          <div className="col-6">
-                            <button
-                              data={product.id}
-                              className="btn deleteBTN"
-                              onClick={deleteProduct}
-                            >
-                              <MdDelete /> Delete
-                            </button>
-                          </div>
-                          <div className="col-6 text-end">
-                            <div className="d-flex justify-content-end">
-                              <button
-                                data={product.id}
-                                className="btn add-minus"
-                                onClick={productMinus}
-                                disabled={product.nbrProduct === 1}
-                              >
-                                -
-                              </button>
-                              <p className="ms-3 me-3">{product.nbrProduct} </p>
-                              <button
-                                data={product.id}
-                                className="btn add-minus"
-                                onClick={productPlus}
-                              >
-                                +
-                              </button>
                             </div>
                           </div>
                         </div>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            </div>
-            <div className="col-12 col-lg-4">
-              <div className="card m-3 ms-lg-0">
-                <div className="card-header">Total</div>
-                <div className="card-body">
-                  <div className="d-flex justify-content-between">
-                    <p>Subtotal</p>
-                    <p>{payDetails.subtotal} TND</p>
-                  </div>
-                  <div className="d-flex justify-content-between">
-                    <p>Discout</p>
-                    <p>{payDetails.discount} TND</p>
-                  </div>
-                  <div className="d-flex justify-content-between">
-                    <p>Coupon</p>
-                    <p>{payDetails.coupon} TND</p>
-                  </div>
-                  <div className="d-flex justify-content-between fw-bold">
-                    <p>Total</p>
-                    <p>{payDetails.total} TND</p>
-                  </div>
-                  <div className="text-center">
-                    <button className="btn btn-checkout">Checkout</button>
+                      );
+                    })}
                   </div>
                 </div>
               </div>
+              <div className="col-12 col-lg-4">
+                <div className="card m-3 ms-lg-0">
+                  <div className="card-header">Total</div>
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between">
+                      <p>Subtotal</p>
+                      <p>{payDetails.subtotal} TND</p>
+                    </div>
+                    <div className="d-flex justify-content-between">
+                      <p>Discout</p>
+                      <p>{payDetails.discount} TND</p>
+                    </div>
+                    <div className="d-flex justify-content-between">
+                      <p>Coupon</p>
+                      <p>{payDetails.coupon} TND</p>
+                    </div>
+                    <div className="d-flex justify-content-between fw-bold">
+                      <p>Total</p>
+                      <p>{payDetails.total} TND</p>
+                    </div>
+                    <div className="text-center">
+                      <button className="btn btn-checkout">Checkout</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="row">
+              <p className="text-center p-5 fs-4 color-grey">
+                Liste of Product is Empty
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
