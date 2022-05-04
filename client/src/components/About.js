f1c5a45768c057c5d608d1bf4396d3bcc58d62b3
fereszareb <fereszareb@gmail.com>
2022-05-04 02:44:17 +0100

    update about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,34 +1,32 @@
-import { React } from "react";
-import { Accordion } from "react-bootstrap";
+import { React, useEffect, useState } from "react";
+import api from "./../config.service";
 const About = () => {
+  //begin api getAll
+  const [data, setData] = useState("");
+  const retrieveAboutUs = async () => {
+    const response = await api.get("/abousUs");
+    return response.data;
+  };
+  useEffect(() => {
+    const getAboutUs = async () => {
+      const aboutUs = await retrieveAboutUs();
+      if (aboutUs) setData(aboutUs);
+      console.log(aboutUs);
+    };
+    getAboutUs();
+  }, []);
+  //end api getAll
   return (
-    <div>
-      <Accordion>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+    <div className="row justify-content-center mb-5 w-100 mx-0">
+      <div className="col-12 col-md-10 col-lg-9 col-xl-8 ">
+        <div className="text-center titleCarousel">
+          About <b>Us</b>
+        </div>
+        <div
+          className="card p-3 mt-5"
+          dangerouslySetInnerHTML={{ __html: data }}
+        />
+      </div>
     </div>
   );
 };
