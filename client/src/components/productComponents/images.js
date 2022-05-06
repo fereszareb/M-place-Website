7e34a811c3a7e6815c5cf95f914e56eca7d6a501
fereszareb <fereszareb@gmail.com>
2022-05-06 04:57:59 +0100

    update

--- client/src/components/productComponents/images.js
+++ client/src/components/productComponents/images.js
@@ -2,44 +2,32 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "./../../css/product.css";
 
-export default class ImagesProduct extends Component {
-  render() {
-    const settings = {
-      customPaging: function (i) {
-        return (
-          <>
-            <img
-              alt=""
-              className="smallPic"
-              src={"https://picsum.photos/id/" + i + "/500/500"}
-            />
-          </>
-        );
-      },
-      dots: true,
-      dotsClass: "slick-dots slick-thumb",
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    };
-    return (
-      <div>
+const ImagesProduct = ({ listeImage }) => {
+  const settings = {
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div>
+      {listeImage ? (
         <Slider {...settings}>
-          <div>
-            <img alt="" src="https://picsum.photos/id/244/500/500" />
-          </div>
-          <div>
-            <img alt="" src="https://picsum.photos/id/247/500/500" />
-          </div>
-          <div>
-            <img alt="" src="https://picsum.photos/id/248/500/500" />
-          </div>
-          <div>
-            <img alt="" src="https://picsum.photos/id/225/500/500" />
-          </div>
+          {listeImage.map((image, key) => {
+            return (
+              <div key={key}>
+                <img alt="" src={image} />
+              </div>
+            );
+          })}
         </Slider>
-      </div>
-    );
-  }
-}
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default ImagesProduct;
