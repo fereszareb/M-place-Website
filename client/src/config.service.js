e769c9037623efb7c35ece5915b9f20ba7d6669b
fereszareb <fereszareb@gmail.com>
2022-05-08 18:37:48 +0100

    update

--- client/src/config.service.js
+++ client/src/config.service.js
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "http://localhost:5000/",
+  headers: {
+    Authorization: "Bearer " + localStorage.getItem("access_token"),
+  },
 });
